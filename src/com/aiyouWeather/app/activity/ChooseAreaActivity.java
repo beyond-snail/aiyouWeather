a0e59ea214e974cc23f9049cb101549626763a46
wcp <543701603@qq.com>
2016-05-07 19:02:09 +0800

    新增切换城市和手动更新天气的功能

--- src/com/aiyouWeather/app/activity/ChooseAreaActivity.java
+++ src/com/aiyouWeather/app/activity/ChooseAreaActivity.java
@@ -71,13 +71,19 @@ public class ChooseAreaActivity extends Activity{
 	 */
 	private int currentLevel;
 	
+	/**
+	 * 是否从WeatherActivity中跳转过来。
+	 */
+	private boolean isFromWeatherActivity;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+		isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		if (prefs.getBoolean("city_selected", false)){
+		//已经选择了城市且不是从WeatherActivity跳转过来，才会直接跳转到WeatherActivity
+		if (prefs.getBoolean("city_selected", false) && !isFromWeatherActivity){
 			Intent intent = new Intent(this, WeatherActivity.class);
 			startActivity(intent);
 			finish();
@@ -268,6 +274,10 @@ public class ChooseAreaActivity extends Activity{
 		}else if (currentLevel == LEVEL_COUNTRY){
 			queryCities();
 		}else{
+			if (isFromWeatherActivity){
+				Intent intent = new Intent(ChooseAreaActivity.this, WeatherActivity.class);
+				startActivity(intent);
+			}
 			finish();
 		}
 	}
